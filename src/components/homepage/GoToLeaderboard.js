0e218cf39c0d1fe6baaa201fd6f1bb65cf8c0060
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2023-01-04 21:52:52 +0200

    Fix leaderboard button

--- src/components/homepage/GoToLeaderboard.js
+++ src/components/homepage/GoToLeaderboard.js
@@ -1,18 +1,19 @@
 import React from "react";
 import CustomButton from "../common/CustomButton";
-import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function GoToLeaderboard() {
+  const navigate = useNavigate();
   return (
     <div className="w-10/12 mx-auto flex justify-around p-4 border border-gray-300 rounded-lg shadow">
-      <div className="logo-text text-6xl font-black">
+      <div className="logo-text  md:text-6xl sm:text-4xl font-black">
         <span className="text-blue-600">Are you the </span>
         <span className="text-red-600"> Best?</span>
       </div>
       <CustomButton
         styleClass="primary"
         text="Leaderboard"
-        onClick={() => redirect("/leaderboard")}
+        onClick={() => navigate("/leaderboard")}
       />
     </div>
   );
