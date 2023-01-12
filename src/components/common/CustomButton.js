3ae763ea60a5032c7fee633a8c994940cd935c22
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2023-01-12 19:50:09 +0200

    Add hover effect

--- src/components/common/CustomButton.js
+++ src/components/common/CustomButton.js
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 
 function CustomButton({ styleClass, text, onClick }) {
   const styles = {
-    primary: "bg-red-600 text-gray-50 border border-red-600",
-    secondary: "bg-gray-50 text-red-600 border border-red-600",
+    primary: "bg-red-600 hover:bg-red-700 text-gray-50 border border-red-600",
+    secondary:
+      "bg-gray-50 hover:bg-gray-100 text-red-600 border border-red-600",
   };
   const style =
-    "rounded-md px-5 py-2 text-2xl font-sans font-semibold cursor-pointer" +
+    "rounded-md px-5 py-2 text-2xl font-sans font-semibold cursor-pointer hover:shadow-md" +
     " " +
     styles[styleClass];
 
