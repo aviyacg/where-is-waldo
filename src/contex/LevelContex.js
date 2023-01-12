262c2bf698d54473c53921fad125590105cc5065
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2023-01-12 19:47:22 +0200

    Change levelList context to an array

--- src/contex/LevelContex.js
+++ src/contex/LevelContex.js
@@ -1,80 +1,58 @@
 import React, { useContext, useState } from "react";
 
 // Images for the levels
-import level2IMG from "../assets/images/ski-slopes.jpg";
 import level1IMG from "../assets/images/cake-factory.jpg";
+import level2IMG from "../assets/images/ski-slopes.jpg";
 import level3IMG from "../assets/images/beach.jpg";
 
 // Create context objects for the current level and its update function
-const CurrentLevelContext = React.createContext();
-const CurrentLevelUpdateContext = React.createContext();
+const CurrentLevelIdContext = React.createContext();
+const CurrentLevelIdUpdateContext = React.createContext();
 
 // Custom Hook for consuming the current level context
-export function useCurrentLevel() {
-  return useContext(CurrentLevelContext);
+export function useCurrentLevelId() {
+  return useContext(CurrentLevelIdContext);
 }
 
 // Custom Hook for consuming the current level update context
-export function useCurrentLevelUpdate() {
-  return useContext(CurrentLevelUpdateContext);
+export function useCurrentLevelIdUpdate() {
+  return useContext(CurrentLevelIdUpdateContext);
 }
 
 // Provider for the current level context and its update function
 export function CurrentLevelProvider({ children }) {
   const [currentLevel, setCurrentLevel] = useState(null);
   return (
-    <CurrentLevelContext.Provider value={currentLevel}>
-      <CurrentLevelUpdateContext.Provider value={setCurrentLevel}>
+    <CurrentLevelIdContext.Provider value={currentLevel}>
+      <CurrentLevelIdUpdateContext.Provider value={setCurrentLevel}>
         {children}
-      </CurrentLevelUpdateContext.Provider>
-    </CurrentLevelContext.Provider>
+      </CurrentLevelIdUpdateContext.Provider>
+    </CurrentLevelIdContext.Provider>
   );
 }
 
-// Create context objects for the list of levels and its update function
-const LevelsContext = React.createContext();
-const LevelsUpdateContext = React.createContext();
-
-// Initial list of levels
-const levelList = [
+// list of levels
+export const levelList = [
   {
-    level: 1,
+    id: 1,
     image: level1IMG,
     title: "The cake factory",
     characterList: ["waldo", "odlaw", "wenda"],
   },
   {
-    level: 2,
+    id: 2,
     image: level2IMG,
     title: "The ski slopes",
     characterList: ["waldo", "odlaw", "wenda", "wizard"],
   },
   {
-    level: 3,
+    id: 3,
     image: level3IMG,
     title: "The beach",
     characterList: ["waldo", "odlaw", "wenda", "wizard"],
   },
 ];
 
-// Provider for the list of levels context and its update function
-export function LevelListProvider({ children }) {
-  const [levels, setLevels] = useState(levelList);
-  return (
-    <LevelsContext.Provider value={levels}>
-      <LevelsUpdateContext.Provider value={setLevels}>
-        {children}
-      </LevelsUpdateContext.Provider>
-    </LevelsContext.Provider>
-  );
-}
-
-// Custom Hook for consuming the list of levels context
-export function useLevelList() {
-  return useContext(LevelsContext);
-}
-
-// Custom Hook for consuming the list of levels update context
-export function useLevelListUpdate() {
-  return useContext(LevelsUpdateContext);
+export function getCurrentLevel(levelId) {
+  return levelList.find((level) => level.id === levelId) || null;
 }
