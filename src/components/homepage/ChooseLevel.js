0c4d17acfaf7fc3f9f41584aa4c246a021f31cad
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2023-01-12 19:48:25 +0200

    levelList format change

--- src/components/homepage/ChooseLevel.js
+++ src/components/homepage/ChooseLevel.js
@@ -1,25 +1,23 @@
 import React from "react";
 import LevelCard from "../common/LevelCard";
 import { useNavigate } from "react-router-dom";
-
-import { useCurrentLevelUpdate, useLevelList } from "../../contex/LevelContex";
+import { useCurrentLevelIdUpdate, levelList } from "../../contex/LevelContex";
 
 function ChooseLevel() {
-  const levelsList = useLevelList();
-  const currentLevelUpdate = useCurrentLevelUpdate();
+  const currentLevelIdUpdate = useCurrentLevelIdUpdate();
   const navigate = useNavigate();
-  const playLevel = (level) => {
-    currentLevelUpdate(level);
+  const playLevel = (levelId) => {
+    currentLevelIdUpdate(levelId);
     navigate("/game");
   };
   return (
     <div className="grid grid-cols-3 gap-4 justify-items-center p-4">
-      {levelsList.map((level, index) => {
+      {levelList.map((level) => {
         return (
           <LevelCard
-            key={level.level}
+            key={level.id}
             {...level}
-            onClick={() => playLevel(level.level)}
+            onClick={() => playLevel(level.id)}
           />
         );
       })}
