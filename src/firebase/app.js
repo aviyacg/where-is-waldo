dfb6aea025c83eca7093d0762f4a41a38db4eec8
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2023-01-13 05:55:53 +0200

    create firebase app instace

---
+++ src/firebase/app.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyALcDFL9TcscGylrj_dqNkGLWAxbWLPUVA",
+  authDomain: "where-s-waldo-b99c4.firebaseapp.com",
+  projectId: "where-s-waldo-b99c4",
+  storageBucket: "where-s-waldo-b99c4.appspot.com",
+  messagingSenderId: "166170127799",
+  appId: "1:166170127799:web:a06f9426efa4c103c8cc32",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
